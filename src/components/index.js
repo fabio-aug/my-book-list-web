b8238f1b4e8258626697607a92de724321de4b26
Fábio Augusto <fabio.augusto1911@gmail.com>
2023-01-30 18:33:18 -0300

    Tornando o book card global.

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,13 @@
 import Page from './page/Page';
 import Modal from './modal/Modal';
 import Divider from './divider/Divider';
+import BookCard from './bookCard/BookCard';
 import SkeletonCard from './skeletonCard/SkeletonCard';
 
 export {
     Page,
     Modal,
     Divider,
+    BookCard,
     SkeletonCard
 }
