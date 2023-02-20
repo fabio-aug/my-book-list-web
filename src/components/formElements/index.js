eea1e4d2aadc9eb8b61086eb65ad387b2f0e84ec
Fábio Augusto <fabio.augusto1911@gmail.com>
2023-02-20 18:19:04 +0000

    Remove x-mui-datepicker.

--- src/components/formElements/index.js
+++ src/components/formElements/index.js
@@ -1,9 +1,7 @@
 import FormikInput from './FormikInput';
 import FormikSelect from './FormikSelect';
-import FormikDatepicker from './FormikDatepicker';
 
 export {
     FormikInput,
-    FormikSelect,
-    FormikDatepicker
+    FormikSelect
 }
